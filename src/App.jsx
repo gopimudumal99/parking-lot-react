4de3865c6eb7da657c461c87412115de2234cad0
Perennial <gopimudumal99@gmail.com>
2022-06-17 18:04:20 +0530

    adding the exit and entry form

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import React,{useState} from 'react'
 import ParkingSpace from './components/ParkingSpace/ParkingSpace';
 import EntryData from './components/Registration/EntryData';
 import ExitVehicle from './components/Exit/ExitVehicle';
+import ChargeAmmount from './components/Charges/ChargeAmmount';
+
 
 function App() {
   const[isEntryData,setIsEntryData] = useState(false)
   const[isExit,setIsExit] = useState(false)
+  const [isAmount,setIsAmount] = useState(false)
 
   console.log(isEntryData)
   return (
     <div className="App">
-      
     {
-      !isEntryData ? !isExit ?<ParkingSpace setIsEntryData={setIsEntryData} setIsExit={setIsExit}/>:<ExitVehicle setIsExit={setIsExit}/> :<EntryData setIsEntryData={setIsEntryData}/>
+      !isEntryData ? !isExit ?isAmount? <ChargeAmmount setIsAmount={setIsAmount}/>:<ParkingSpace setIsEntryData={setIsEntryData} setIsExit={setIsExit}/>:<ExitVehicle setIsExit={setIsExit}/> :<EntryData setIsEntryData={setIsEntryData}/>
     }
-    
     </div>
   );
 }
---
+++ src/components/Charges/ChargeAmmount.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./ChargeAmount.css"
+function ChargeAmmount() {
+  return (
+ <div className='charge-container'>
+    <div className='corss-icon'>&#x2718;</div>
+     <div className='charge-details'>
+        <div><span>Owner name :</span>Gopi</div>
+        <div><span>Vehicle-type :</span> 4 wheeler</div>
+        <div><span>Total Time in hrs :</span> 2hr</div>
+        <div><span>Total Amount :</span> ₹50.00</div>
+        <h2>Thank You! </h2>
+     </div>
+</div>
+  )
+}
+
+export default ChargeAmmount
